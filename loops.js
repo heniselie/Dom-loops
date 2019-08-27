dda19a2c73c82c6d1795220929044f7d107a9cb5
henok tessema <heniselie@gmail.com>
2019-08-26 20:23:59 -0500

    added new files

---
+++ loops.js
@@ -0,0 +1,14 @@
+console.log('Dom-loop');
+const instructors = [
+    {first: "Zoee", last: "Amese"},
+    {first: "Callan", last: "Morrison"},
+    {first: "Greg", last: "Korte"},
+    {first: "Michael", last: "Clarkeeeeeee"},
+  ];
+  
+const printNames = (namesArray) => {
+    for (let i = 0; i < namesArray.length;i++) {
+        const person =namesArray[i];
+        const element = `<p>firstName lastName</p>`
+    }
+}
